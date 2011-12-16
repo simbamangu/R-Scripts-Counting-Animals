5545c88a7c7d0d8617d1af9f52653ffc3bc8870d
Howard Frederick <simbamangu@gmail.com>
2011-12-16 07:59:30 +0300

    rotate points shapefile
    Script for arbitrary oration of point shapefile

---
+++ rotatePoints.R
@@ -0,0 +1,19 @@
+rotatePoints <- function(shfile, rotation){
+  rotshape <- readShapePoints(shfile)
+  SP <- as(rotshape, "SpatialPoints")
+  rotobj <- as(SP, "ppp")
+
+  xvec <- rotobj$x
+  yvec <- rotobj$y
+	xmin <- min(xvec)
+	ymin <- min(yvec)
+  xmax <- max(xvec)
+  ymax <- max(yvec)
+	xvec <- xvec - xmin - (ymax - ymin) / 2
+	yvec <- yvec - ymin - (xmax - xmin) / 2
+	rotR <- (rotation * pi) / 180
+	curr.ppp <- ppp(xvec, yvec, window = owin(c(min(xvec), max(xvec)), c(min(yvec), max(yvec))))
+	curr.ppp <- rotate.ppp(curr.ppp, rotR)
+
+	return(data.frame(cbind(curr.ppp$x + xmin + (xmax - xmin)/2, curr.ppp$y + ymin + (ymax - ymin) / 2)))
+	}
